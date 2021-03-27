aaaa14bcf8481965aa4db007ae0af6d93a8d5da9
Gustavo <gujoseleite@gmail.com>
2021-03-27 16:47:34 -0300

    Changing model and training

--- models.py
+++ models.py
@@ -21,35 +21,30 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
-        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 128, 3)
+        self.conv3 = nn.Conv2d(128, 256, 1)
+             
+        self.fc1 = nn.Linear(256*27*27, 512)        
+        self.fc2 = nn.Linear(512, 68*2)
 
-        self.conv2 = nn.Conv2d(32, 256, 5)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        
-        self.conv3 = nn.Conv2d(256, 512, 5)
-        self.pool3 = nn.MaxPool2d(2, 2)
-        
-        self.fc1 = nn.Linear(512*24*24, 256)
-        self.dropout = nn.Dropout(p=0.2)
-        self.fc2 = nn.Linear(256, 136)
-        
+        self.dropout = nn.Dropout(p=0.3)
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
                 
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
         
         x = x.view(x.size(0), -1)
         
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
-        
-        x = F.log_softmax(x, dim=1)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
