ad4a1104de00253d9f690190d44a543e949fb147
Gustavo <gujoseleite@gmail.com>
2021-03-31 06:00:41 -0300

    Update model

--- models.py
+++ models.py
@@ -22,14 +22,16 @@ class Net(nn.Module):
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 128, 3)
-        self.conv3 = nn.Conv2d(128, 256, 1)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        self.conv4 = nn.Conv2d(128, 256, 5)
+        self.conv5 = nn.Conv2d(256, 512, 5)
              
-        self.fc1 = nn.Linear(256*27*27, 512)        
-        self.fc2 = nn.Linear(512, 68*2)
+        self.fc1 = nn.Linear(512*3*3, 12800)        
+        self.fc2 = nn.Linear(12800, 68*2)
 
-        self.dropout = nn.Dropout(p=0.3)
-
+        self.dropout = nn.Dropout(p=0.25)
+        
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
                 
@@ -37,8 +39,15 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout(x)
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout(x)
         x = self.pool(F.relu(self.conv3(x)))
+        x = self.dropout(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.dropout(x)
+        x = self.pool(F.relu(self.conv5(x)))
+        x = self.dropout(x)
         
         x = x.view(x.size(0), -1)
         
@@ -48,5 +57,3 @@ class Net(nn.Module):
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
-
-
